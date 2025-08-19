0bb924a16a75c16b4428abba364e398f63432abf
ketlymarim <ketlymarim.cursos@gmail.com>
2025-08-18 22:17:57 -0300

    Creating main loop Connecting Menu anda Game classes

--- code/game.py
+++ code/game.py
@@ -3,20 +3,16 @@
 import pygame
 
 from code.menu import Menu
+from const import WIN_HEIGHT, WIN_WIDTH
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, WID_WIDTH=None):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
-
-            # Check for all events
-            # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-            #        pygame.quit()  # Close window
-            #        quit()  # end pygame
+            pass
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close window
+                    quit()  # end pygame
